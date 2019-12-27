d683f28e1b92e9e6ee69e63cc0d29be29c11a13b
Oranjos9 <bscnrb164716@spu.ac.ke>
2019-12-27 18:50:25 +0300

    Adds logign and register component, adds authentification, adds auth guard and token interceptor

--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -4,6 +4,7 @@ import {Routes, RouterModule} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
 import {NgPipesModule} from 'ngx-pipes';
 import {MapModule} from '../common/map/map.module';
+import { Daterangepicker } from 'ng2-daterangepicker';
 
 
 import { RentalListComponent } from './rental-list/rental-list.component';
@@ -14,13 +15,15 @@ import {RentalService} from './shared/rental.service';
 import { RentalDetailComponent } from './rental-detail/rental-detail.component';
 import {UppercasePipe} from '../common/pipes/uppercase.pipe';
 
+import {AuthGuard} from '../auth/shared/auth.guard';
+import { RentalDetailBookingComponent } from './rental-detail/rental-detail-booking/rental-detail-booking.component';
 
 const routes: Routes =[
 {path: 'rentals', 
 component: RentalComponent,
 children:[
   {path:'',component:RentalListComponent},
-  {path:':rentalId', component:RentalDetailComponent}
+  {path:':rentalId', component:RentalDetailComponent, canActivate:[AuthGuard]}
 ]},
 
 ]
@@ -32,7 +35,8 @@ children:[
        RentalListItemComponent,
        RentalComponent,
        RentalDetailComponent,
-       UppercasePipe
+       UppercasePipe,
+       RentalDetailBookingComponent
 
 	],
 	imports: [
@@ -40,7 +44,8 @@ children:[
 	 RouterModule.forChild(routes),
 	 HttpClientModule,
 	 NgPipesModule,
-	 MapModule
+	 MapModule,
+	 Daterangepicker
 	 ],
 	providers:[RentalService]
 })
